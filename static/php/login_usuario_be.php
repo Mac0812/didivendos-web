52e72bd1e8811cad2c848c4c1e93376359995919
Mac0812 <mmancilla1800@alumno.ipn.mx>
2024-06-24 23:19:20 -0600

    se cambio es estilo de la la pagina de registro e inicio de sesion

---
+++ static/php/login_usuario_be.php
@@ -0,0 +1,30 @@
+<?php
+  session_start();
+include 'conexion_be.php';
+$correo = $_POST ['correo'];
+$contrasena = $_POST ['contrasena'];
+$contrasena = hash('sha512',$contrasena);
+
+//verificar los datos que esten registrados
+$validar_login = mysqli_query( $conexion, "SELECT * FROM usuarios WHERE correo ='$correo' 
+and contrasena='$contrasena'");
+
+
+
+if (mysqli_num_rows($validar_login) > 0){
+    $_SESSION['usuario'] = $correo;
+    header ("location:../index.php");
+    exit;
+} 
+else{
+
+    echo '
+    <script>
+    alert("Correo o contraseña incorrectos");
+    window.location = "../pages-sign-in.php";
+    </script>
+    ';
+    exit;
+}
+
+?>
--- static/php/registro_usuario_be.php
+++ static/php/registro_usuario_be.php
@@ -6,15 +6,44 @@ $correo = $_POST['correo'];
 $usuario = $_POST['usuario'];
 $contrasena = $_POST['contrasena'];
 
+
+//se va a encriptar la contraseña
+$contrasena = hash('sha512',$contrasena);
+
 $query = "INSERT INTO usuarios (nombre_completo, correo, usuario, contrasena) 
 VALUES ('$nombre_completo', '$correo', '$usuario', '$contrasena')";
+
+//verificar  que el correo no se repita en la base de datos
+$verificar_correo = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo'");
+if(mysqli_num_rows($verificar_correo) > 0){
+    echo '
+        <script>
+        alert("Este correo ya esta registrado, intenta con otro correo diferente");
+         window.location = "../pages-sign-in.php";
+    </script>
+    ';
+    exit();
+}
+
+//verificar  que el nombre de usuario no se repita en la base de datos
+$verificar_usuario = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario='$usuario'");
+if(mysqli_num_rows($verificar_usuario) > 0){
+    echo '<script>
+        alert("Este usuario ya esta registrado, intenta con otro correo diferente");
+         window.location = "../pages-sign-in.php";
+    </script>
+    ';
+    exit();
+}
+
+
 
 $resultado = mysqli_query($conexion, $query);
 
 if ($resultado) {
    echo 
     '<script>
-        alert("Usuario almacenado");
+        alert("Usuario registrado");
          window.location = "../pages-sign-in.php";
     </script>'
    ;
