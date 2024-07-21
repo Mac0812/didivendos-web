e60bbeaad80b798efeeb08724553c35855020133
KarlaCardozo <kcardozor1600@alumno.ipn.mx>
2024-07-20 19:24:00 -0600

    Implementacion logos

--- static/php/get_companies.php
+++ static/php/get_companies.php
@@ -1,8 +1,5 @@
 <?php
-$conexion = mysqli_connect("localhost", "root", "", "p4t5z8n7_dividendoMX");//ya se hizo la conexion
-if (mysqli_connect_errno()) {
-    die("Conexi��n fallida: " . mysqli_connect_error());
-}
+include 'conexion_be.php';
 
 
 $sql = "SELECT * FROM empresas";
