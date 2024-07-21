067d3ff6914c8137794fc518b0ac0bbe6c94dea9
Mac0812 <mmancilla1800@alumno.ipn.mx>
2024-07-21 00:52:44 -0600

    mood oscuro admin

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
 const btnSwitch = document.querySelector('#switch');
-                btnSwitch.addEventListener('click', ()=>{
-                    document.body.classList.toggle('dark');
-                    btnSwitch.classList.toggle('active');
-                });
+btnSwitch.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  btnSwitch.classList.toggle('active');
+});
